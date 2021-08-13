e4df35aee0a540cfe771ad3bb6fb7faf4d75b94d
Enchoz <oziomaene@gmail.com>
2021-08-13 09:20:06 +0100

    Created CustomerInformation.API project

---
+++ CustomerInformation2.API/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using CustomerInformation2.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using CustomerInformation2.Core.Services;
+using CustomerInformation2.Core.Interfaces;
+using CustomerInformation2.Infrastructure.Data;
+
+namespace CustomerInformation2.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<CustomerInformationContext>(options => options.UseInMemoryDatabase("CustomerInformationList"));
+            services.AddScoped<ICustomerInformationService, CustomerInformationService>();
+            services.AddScoped<IDataCommand, DataCommand>();
+            services.AddScoped<IDataQuery, DataQuery>();
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
